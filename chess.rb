ad8b83b4c622b98aec74efca493e18b701c87229
Pavol Vargovcik <pallly.vargovcik@gmail.com>
2014-11-22 16:32:57 +0100

    functions for initialization of chess desk

--- chess.rb
+++ chess.rb
@@ -2,71 +2,84 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './chessdesk'
 require 'data_mapper'
-require 'pry'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] ||
                            "sqlite3://#{Dir.pwd}/chess.db")
 
 class Piece
   include DataMapper::Resource
-  
+
   property :id, Serial
   property :x, Integer
   property :y, Integer
-  
+
   belongs_to :type
 end
 
 
 class Type
   include DataMapper::Resource
-  
+
   property :id, Serial
   property :type, String
   property :colour, String
   property :path, String
-  
+
   has n, :pieces
 end
 
 DataMapper.finalize
 
+TYPES = ['rook', 'knight', 'bishop', 'queen',
+         'king', 'bishop', 'knight', 'rook', 'pawn']
+
 def chess_init
   Piece.all.each do |piece|
     piece.destroy
   end
-  
-  Piece.create(
-    x:    3,
-    y:    7,
-    type: Type.first(colour: "white", type: "queen")
-  )
-  
-  Piece.create(
-    x:    0,
-    y:    0,
-    type: Type.first(colour: "black", type: "rook")
-  )
+
+  [[1, 'black'], [6, 'white']].each do |y, colour|
+    (0..7).each do |x|
+      Piece.create(
+        x:    x,
+        y:    y,
+        type: Type.first(colour: colour, type: "pawn")
+      )
+    end
+  end
+
+  [[0, 'black'], [7, 'white']].each do |y, colour|
+    TYPES.select{|x| x != 'pawn'}.each_with_index do |type, x|
+      Piece.create(
+        x:    x,
+        y:    y,
+        type: Type.first(colour: colour, type: type)
+      )
+    end
+  end
 end
 
 def types_init
   Type.all.each do |type|
     type.destroy
   end
-  
-  Type.create(
-    colour: "white",
-    type:   "queen",
-    path:   "pieces/queen_white.png"
-  )
-  
-  Type.create(
-    colour: "black",
-    type:   "rook",
-    path:   "pieces/rook_black.png"
-  )
+
+  TYPES.each do |type|
+    ['white', 'black'].each do |colour|
+      Type.create(
+        colour: colour,
+        type:   type,
+        path:   "pieces/#{type}_#{colour}.png"
+      )
+    end
+  end
 end
 
+def init
+  DataMapper.auto_migrate!
+  types_init
+  chess_init
+end
 
 get '/' do
   pieces = Piece.all.to_a
@@ -79,14 +92,14 @@ post '/' do
   pieces = Piece.all.to_a
   x = params['cell'][0].to_i
   y = params['cell'][2].to_i
-  
+
   piece = nil
   pieces.each do |p|
     if p.x == x and p.y == y
       piece = p
     end
   end
-  
+
   erb(:'index.html', locals: {pieces: pieces,
                               selected: piece,
                               miss: piece == nil})
